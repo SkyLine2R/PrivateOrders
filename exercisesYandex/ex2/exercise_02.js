5e3f22a5d9b36b5e22381501616c64b5cf46f090
Oleg Vasilenko <oleg.vasilenko@gmail.com>
2023-02-15 00:10:55 +0300

    Синхрон задания

--- exercisesYandex/ex2/exercise_02.js
+++ exercisesYandex/ex2/exercise_02.js
@@ -1,59 +1,75 @@
 const fs = require("fs");
 const inputFileName = "./input.txt";
 
-function beautifulString(numberOfSubstitutions, textString) {
-  console.log(numberOfSubstitutions);
-  console.log(textString);
+function searchBeautifulString(quantityOfchange, textString) {
+  quantityOfchange = +quantityOfchange;
 
-  const uniqueLetters = new Set(...textString.split("")).size;
-  const textStringLenght = textString.lenght;
+  console.log("Количество изменений:" + quantityOfchange);
+  console.log("Строка: " + textString);
+
+  const quantityUnqLtrs = new Set(textString.split("")).size; //уникальные буквы
+  const textStrLnth = textString.length; //длина строки
+
+  console.log(quantityUnqLtrs + quantityOfchange >= textStrLnth);
 
   if (
-    textStringLenght <= numberOfSubstitutions ||
-    textStringLenght == numberOfSubstitutions + 1
+    textStrLnth <= quantityOfchange ||
+    textStrLnth == quantityOfchange + 1 ||
+    quantityUnqLtrs + quantityOfchange >= textStrLnth ||
+    quantityUnqLtrs === 1
   )
-    return textStringLenght;
+    return textStrLnth;
 
-  if (uniqueLetters === 1) return textStringLenght;
+  let startSearchPos = 1;
+  let currentSearchSymbolPos = 1;
+  let restQuantityOfchange = quantityOfchange;
+  let lengthOfBeatifulStr = 1;
+  let currentLengthOfBeatifulStr = 1;
+  let searchSymbol = textString[0];
+  let beatifulLine = false;
 
-  if (uniqueLetters === textStringLenght) return numberOfSubstitutions + 1;
-
-  let searchSymbolPos = 1;
-  let restChangeLatters = numberOfSubstitutions;
-  let krasotaSearch = 1;
-
-  while (
-    searchSymbolPos < textStringLenght &&
-    (restChangeLatters > 0 || beatifulLine)
-  ) {
-    searchSymbol = textString[searchSymbolPos - 1];
-
-    if (textString[searchSymbolPos] == searchSymbol) {
-      krasotaSearch++;
-      beatifulLine = true;
-    } else restChangeLatters--;
+  /*  while (textStrLnth - startSearchPos > lengthOfBeatifulStr) { */
+  while (startSearchPos < textStrLnth) {
+    restQuantityOfchange = quantityOfchange;
+    while (
+      currentSearchSymbolPos < textStrLnth && ///возможно добавить единицу к текущей, для увел. скорости
+      (restQuantityOfchange > 0 || beatifulLine)
+    ) {
+      console.log("currentSearchSymbolPos: " + currentSearchSymbolPos);
+      console.log("restQuantityOfchange: " + restQuantityOfchange);
+      if (textString[currentSearchSymbolPos] == searchSymbol) {
+        currentLengthOfBeatifulStr++;
+        beatifulLine = true;
+      } else {
+        restQuantityOfchange--;
+        beatifulLine = false;
+      }
+      currentSearchSymbolPos++;
+    }
+    if (lengthOfBeatifulStr < currentLengthOfBeatifulStr)
+      //текущая лучшая строка
+      lengthOfBeatifulStr = currentLengthOfBeatifulStr;
+    while (
+      //подбор позиции следующего символа, чтобы это был отличный от уже пройденного
+      searchSymbol == textString[startSearchPos - 1] &&
+      startSearchPos + 1 < textStrLnth
+    ) {
+      startSearchPos++;
+    }
+    searchSymbol = textString[currentSearchSymbolPos - 1];
+    currentSearchSymbolPos = startSearchPos;
+    beatifulLine = false;
   }
+  return lengthOfBeatifulStr + "";
 }
-startSearchPosition = 1;
-
-console.log();
-
-/* function choiceOfOptions(
-   
-  string,
-  searchPosition,
-  numberOfCoincidences,
-  numberOfSubstitutions,
-  leftToReplace
-) 
- */
 
 fs.readFile(inputFileName, (errRead, data) => {
   if (errRead) {
     throw errRead;
   }
-  const dataToSave = beautifulString(...data.toString().split("\n"));
-  process.stdout.write(dataToSave);
+  //console.log(searchBeautifulString(...data.toString().split("\n")));
+  const dataToSave = searchBeautifulString(...data.toString().split("\n"));
+  process.stdout.write(dataToSave + "");
 });
 
 // let inputString = "";

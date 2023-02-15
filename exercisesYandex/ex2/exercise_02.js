591e3c14443504bb6a979d93bb29ec5b32efd91a
SkyLine2R <oleg.vasilenko@gmail.com>
2023-02-15 16:53:02 +0300

    Ничего

--- exercisesYandex/ex2/exercise_02.js
+++ exercisesYandex/ex2/exercise_02.js
@@ -1,75 +1,92 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable spaced-comment */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-plusplus */
+/* eslint-disable prefer-template */
 const fs = require("fs");
+
 const inputFileName = "./input.txt";
 
 function searchBeautifulString(quantityOfchange, textString) {
-  quantityOfchange = +quantityOfchange;
-
-  console.log("Количество изменений:" + quantityOfchange);
-  console.log("Строка: " + textString);
-
-  const quantityUnqLtrs = new Set(textString.split("")).size; //уникальные буквы
-  const textStrLnth = textString.length; //длина строки
-
-  console.log(quantityUnqLtrs + quantityOfchange >= textStrLnth);
+  const quantityUnqLtrs = new Set(textString.split("")).size; // уникальные буквы
+  const textStrLnth = textString.length; // длина строки
 
   if (
     textStrLnth <= quantityOfchange ||
     textStrLnth == quantityOfchange + 1 ||
-    quantityUnqLtrs + quantityOfchange >= textStrLnth ||
     quantityUnqLtrs === 1
   )
     return textStrLnth;
 
-  let startSearchPos = 1;
-  let currentSearchSymbolPos = 1;
-  let restQuantityOfchange = quantityOfchange;
-  let lengthOfBeatifulStr = 1;
-  let currentLengthOfBeatifulStr = 1;
-  let searchSymbol = textString[0];
-  let beatifulLine = false;
+  const beatiful1 = search(textString);
+  const beatiful2 = search(textString.split("").reverse().join(""));
+  console.log(beatiful2);
+  console.log(beatiful1);
 
-  /*  while (textStrLnth - startSearchPos > lengthOfBeatifulStr) { */
-  while (startSearchPos < textStrLnth) {
-    restQuantityOfchange = quantityOfchange;
-    while (
-      currentSearchSymbolPos < textStrLnth && ///возможно добавить единицу к текущей, для увел. скорости
-      (restQuantityOfchange > 0 || beatifulLine)
-    ) {
-      console.log("currentSearchSymbolPos: " + currentSearchSymbolPos);
-      console.log("restQuantityOfchange: " + restQuantityOfchange);
-      if (textString[currentSearchSymbolPos] == searchSymbol) {
-        currentLengthOfBeatifulStr++;
-        beatifulLine = true;
-      } else {
-        restQuantityOfchange--;
-        beatifulLine = false;
+  return beatiful1 > beatiful2 ? beatiful1 : beatiful2;
+
+  function search(textString) {
+    console.log("Количество изменений:" + quantityOfchange);
+    console.log("Строка: " + textString);
+
+    let startSearchPos = 0;
+    let lengthOfBeatifulStr = 1;
+
+    while (startSearchPos + 1 < textStrLnth - quantityOfchange) {
+      let restQuantityOfchange = quantityOfchange;
+      let currentLnthOfBeatifulStr = 1;
+      let lastChance = true;
+      const searchSymbol = textString[startSearchPos];
+
+      let currentSearchSymbolPos = startSearchPos + 1;
+
+      console.log("   ");
+      console.log("Старт поиска с нового символа");
+      console.log("startSearchPos" + startSearchPos);
+
+      console.log("searchSymbol " + searchSymbol);
+
+      while (currentSearchSymbolPos + 1 < textStrLnth && lastChance) {
+        console.log("currentLnthOfBeatifulStr " + currentLnthOfBeatifulStr);
+        console.log("currentSearchSymbolPos: " + currentSearchSymbolPos);
+        console.log("restQuantityOfchange: " + restQuantityOfchange);
+
+        if (textString[currentSearchSymbolPos] == searchSymbol) {
+          currentLnthOfBeatifulStr++;
+        } else if (restQuantityOfchange) {
+          restQuantityOfchange--;
+          currentLnthOfBeatifulStr++;
+        } else lastChance = false;
+
+        currentSearchSymbolPos++;
       }
-      currentSearchSymbolPos++;
-    }
-    if (lengthOfBeatifulStr < currentLengthOfBeatifulStr)
-      //текущая лучшая строка
-      lengthOfBeatifulStr = currentLengthOfBeatifulStr;
-    while (
-      //подбор позиции следующего символа, чтобы это был отличный от уже пройденного
-      searchSymbol == textString[startSearchPos - 1] &&
-      startSearchPos + 1 < textStrLnth
-    ) {
+      if (lengthOfBeatifulStr < currentLnthOfBeatifulStr)
+        lengthOfBeatifulStr = currentLnthOfBeatifulStr; // текущая лучшая строка
+
       startSearchPos++;
+
+      while (
+        // подбор позиции следующего стартового символа, чтобы это был отличный от уже пройденного
+        searchSymbol == textString[startSearchPos] &&
+        startSearchPos < textStrLnth - quantityOfchange
+      ) {
+        startSearchPos++;
+      }
+      console.log("startSearchPos " + startSearchPos);
     }
-    searchSymbol = textString[currentSearchSymbolPos - 1];
-    currentSearchSymbolPos = startSearchPos;
-    beatifulLine = false;
+    return lengthOfBeatifulStr;
   }
-  return lengthOfBeatifulStr + "";
 }
 
 fs.readFile(inputFileName, (errRead, data) => {
   if (errRead) {
     throw errRead;
   }
-  //console.log(searchBeautifulString(...data.toString().split("\n")));
-  const dataToSave = searchBeautifulString(...data.toString().split("\n"));
-  process.stdout.write(dataToSave + "");
+  const quantityChange = +data.toString().split("\n")[0];
+  const searchString = data.toString().split("\n")[1];
+
+  const dataToSave = searchBeautifulString(quantityChange, searchString) + "";
+  process.stdout.write(dataToSave);
 });
 
 // let inputString = "";

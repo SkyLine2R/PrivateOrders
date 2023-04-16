95fafc613d7e84c2eeaea5094cba4671abd82053
Oleg Vasilenko <oleg.vasilenko@gmail.com>
2023-04-16 23:32:49 +0300

    $

--- client-react/src/menu-app-bar/menu-app-bar.js
+++ client-react/src/menu-app-bar/menu-app-bar.js
@@ -10,9 +10,40 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import Button from "@mui/material/Button";
+import StorageIcon from "@mui/material/StorageIcon";
+
+const pages = ["Склад", "Поступления", "Списания"];
 
 export default function MenuAppBar() {
-  const [auth, setAuth] = React.useState(true);
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">Учет складских материалов</Typography>
+          <Button color="inherit" startIcon={<StorageIcon />}>
+            Склад
+          </Button>
+          <Button color="inherit" startIcon={<ArrowDownwardIcon />}>
+            Приход
+          </Button>
+          <Button color="inherit" startIcon={<ArrowUpwardIcon />}>
+            Расход
+          </Button>
+          <IconButton color="inherit" aria-label="account">
+            {/*             <AccountCircleIcon />
+             */}{" "}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+
+  /* const [auth, setAuth] = React.useState(true);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   const handleMenu = (event) => {
@@ -21,6 +52,13 @@ export default function MenuAppBar() {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
   };
 
   return (
@@ -39,6 +77,32 @@ export default function MenuAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Photos
           </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <Menu
+              id="menu-appbar-nav"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
           {auth && (
             <div>
               <IconButton
@@ -52,7 +116,7 @@ export default function MenuAppBar() {
                 <AccountCircle />
               </IconButton>
               <Menu
-                id="menu-appbar"
+                id="menu-appbar-auth"
                 anchorEl={anchorEl}
                 anchorOrigin={{
                   vertical: "top",
@@ -74,5 +138,5 @@ export default function MenuAppBar() {
         </Toolbar>
       </AppBar>
     </Box>
-  );
+  ); */
 }
